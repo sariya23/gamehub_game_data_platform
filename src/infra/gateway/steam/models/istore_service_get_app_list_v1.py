3f4e8ee927ae0e002d1cf43861331e35f41fac75
nikita <dayzwaitik123@yandex.ru>
2026-08-20 01:12:46 +0300

    raw response

--- src/infra/gateway/steam/models/istore_service_get_app_list_v1.py
+++ src/infra/gateway/steam/models/istore_service_get_app_list_v1.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class IStoreServiceGetAppListV1ResponseSteamApp(BaseModel):
-    app_id: int = Field(validation_alias="appid")
+    app_id: int
     name: str
     last_modified: int
     price_change_number: int
--- src/infra/gateway/steam/steam.py
+++ src/infra/gateway/steam/steam.py
@@ -7,7 +7,6 @@ from src.infra.gateway.steam.constants import (
 )
 from src.infra.gateway.steam.models.istore_service_get_app_list_v1 import (
     IStoreServiceGetAppListV1RequestDTO,
-    IStoreServiceGetAppListV1ResponseDTO,
 )
 from src.types import Seconds
 
@@ -20,7 +19,7 @@ class SteamApi:
     def isotre_service_get_app_list_v1(
         self,
         request: IStoreServiceGetAppListV1RequestDTO,
-    ) -> IStoreServiceGetAppListV1ResponseDTO:
+    ) -> dict[str, object]:
         url_path = f"/{STEAM_API_INTERFACE_STORE_SERVICE}/{STEAM_API_METHOD_GET_APP_LIST}/{STEAM_API_VERSION_V1}/"
         response = self.__client.get(
             url=url_path,
@@ -28,4 +27,4 @@ class SteamApi:
         )
         response.raise_for_status()
 
-        return IStoreServiceGetAppListV1ResponseDTO.model_validate_json(response.content)
+        return response.json()
